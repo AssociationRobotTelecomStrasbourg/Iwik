1586063f394b4b4cd6b5c15a8df4b11d8a6b877d
Jonathan Plasse <13716151+JonathanPlasse@users.noreply.github.com>
2019-10-17 20:07:12 +0200

    Now use AccelStepper

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 #include <Arduino.h>
-#include "TMC2130Stepper.h"
-#include "protoPins.h"
 #include <TMC2130Stepper.h>
+#include <AccelStepper.h>
+#include "protoPins.h"
 
-TMC2130Stepper DRV2 = TMC2130Stepper(EN2, DIR2, STEP2, CS2, SDI, SDO, SCK);
-TMC2130Stepper DRV1 = TMC2130Stepper(EN1, DIR1, STEP1, CS1, SDI, SDO, SCK);
-bool dir = true;
+TMC2130Stepper stepper_config1 = TMC2130Stepper(EN1, DIR1, STEP1, CS1, SDI, SDO, SCK);
+TMC2130Stepper stepper_config2 = TMC2130Stepper(EN2, DIR2, STEP2, CS2, SDI, SDO, SCK);
+
+AccelStepper stepper1(1, STEP1, DIR1);
+AccelStepper stepper2(1, STEP2, DIR2);
+
+uint32_t stepsPerSecond = 40000;
+uint32_t stepsPerSecondSquared = 100000;
+int32_t distance = 50000;
+
+int8_t turn = 1;
 
 void setup() {
 
@@ -15,48 +23,36 @@ void setup() {
 	delay(10);
 	//Initialize SPI, set current to 600mA, and activate stealthchop for both drivers
 	//Both drivers need to be initialized even if only one is used
-	DRV2.begin();
-	DRV1.begin();
+	stepper_config1.begin();
+	stepper_config2.begin();
 	delay(10);
 
-	DRV2.SilentStepStick2130(600);
-	DRV1.SilentStepStick2130(600);
+	stepper_config1.SilentStepStick2130(600);
+	stepper_config2.SilentStepStick2130(600);
 	delay(10);
 
-	DRV1.stealthChop(1);
-	DRV2.stealthChop(1);
+	stepper_config1.stealthChop(1);
+	stepper_config2.stealthChop(1);
 	delay(10);
 
-	digitalWrite(EN2, LOW); //Turning on the output stage
-	digitalWrite(EN1, LOW);
+	stepper1.setMaxSpeed(stepsPerSecond);
+	stepper2.setMaxSpeed(stepsPerSecond);
+
+	stepper1.setAcceleration(stepsPerSecondSquared);
+	stepper2.setAcceleration(stepsPerSecondSquared);
+
+	digitalWrite(EN1, LOW); //Turning on the output stage
+	digitalWrite(EN2, LOW);
 
 	delay(3000);
 }
 
 void loop() {
-
-	//Rotating both motors alternatively in both directions every 2 seconds
-	digitalWrite(STEP2, HIGH);
-	digitalWrite(STEP1, HIGH);
-
-	delayMicroseconds(10);
-
-	digitalWrite(STEP2, LOW);
-	digitalWrite(STEP1, LOW);
-
-	delayMicroseconds(10);
-
-	uint32_t ms = millis();
-	static uint32_t last_time = 0;
-	if ((ms - last_time) > 2000) {
-		if (dir) {
-			DRV2.shaft_dir(1);
-			DRV1.shaft_dir(1);
-		} else {
-			DRV2.shaft_dir(0);
-			DRV1.shaft_dir(0);
-		}
-		dir = !dir;
-		last_time = ms;
+	if (stepper1.distanceToGo() == 0 && stepper2.distanceToGo() == 0) {
+		stepper1.move(-turn * distance);
+		stepper2.move(turn * distance);
+		turn *= -1;
 	}
+	stepper1.run();
+	stepper2.run();
 }
