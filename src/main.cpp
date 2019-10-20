97e7e094bb784e03352b64ca116f7fc562388804
Jonathan Plasse <13716151+JonathanPlasse@users.noreply.github.com>
2019-10-20 14:32:13 +0200

    TeensyStep works !

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,30 @@
 #include <Arduino.h>
 #include <TMC2130Stepper.h>
-#include <AccelStepper.h>
+#include <TeensyStep.h>
 #include "protoPins.h"
 
 TMC2130Stepper stepper_config1 = TMC2130Stepper(EN1, DIR1, STEP1, CS1, SDI, SDO, SCK);
 TMC2130Stepper stepper_config2 = TMC2130Stepper(EN2, DIR2, STEP2, CS2, SDI, SDO, SCK);
 
-AccelStepper stepper1(1, STEP1, DIR1);
-AccelStepper stepper2(1, STEP2, DIR2);
+Stepper stepper1(STEP1, DIR1);
+Stepper stepper2(STEP2, DIR2);
 
-uint32_t stepsPerSecond = 40000;
-uint32_t stepsPerSecondSquared = 100000;
-int32_t distance = 50000;
+StepControl controller;
+
+uint32_t stepsPerSecond = 50000;
+uint32_t stepsPerSecondSquared = 50000;
+int32_t distance = 100000;
 
 int8_t turn = 1;
 
 void setup() {
 
-	//The delays between SPI calls seem to be important for consistent behaviour
-	//Driver Enables are at 1 (3V3) by default, i.e disabled, you need to pull them low to enable the drivers.
+	// The delays between SPI calls seem to be important for consistent behaviour
+	// Driver Enables are at 1 (3V3) by default, i.e disabled, you need to pull them low to enable the drivers.
 
 	delay(10);
-	//Initialize SPI, set current to 600mA, and activate stealthchop for both drivers
-	//Both drivers need to be initialized even if only one is used
+	// Initialize SPI, set current to 600mA, and activate stealthchop for both drivers
+	// Both drivers need to be initialized even if only one is used
 	stepper_config1.begin();
 	stepper_config2.begin();
 	delay(10);
@@ -35,24 +37,27 @@ void setup() {
 	stepper_config2.stealthChop(1);
 	delay(10);
 
-	stepper1.setMaxSpeed(stepsPerSecond);
-	stepper2.setMaxSpeed(stepsPerSecond);
+	// Configure max speed and acceleration
+	stepper1
+		.setMaxSpeed(stepsPerSecond)
+		.setAcceleration(stepsPerSecondSquared)
+		.setInverseRotation(true);
 
-	stepper1.setAcceleration(stepsPerSecondSquared);
-	stepper2.setAcceleration(stepsPerSecondSquared);
+	stepper2
+		.setMaxSpeed(stepsPerSecond)
+		.setAcceleration(stepsPerSecondSquared);
 
-	digitalWrite(EN1, LOW); //Turning on the output stage
+	// Turning on the driver
+	digitalWrite(EN1, LOW);
 	digitalWrite(EN2, LOW);
-
-	delay(3000);
 }
 
 void loop() {
-	if (stepper1.distanceToGo() == 0 && stepper2.distanceToGo() == 0) {
-		stepper1.move(-turn * distance);
-		stepper2.move(turn * distance);
-		turn *= -1;
-	}
-	stepper1.run();
-	stepper2.run();
+	stepper1.setTargetRel(distance);
+	stepper2.setTargetRel(distance);
+	controller.move(stepper1, stepper2);
+
+	stepper1.setTargetRel(-distance);
+	stepper2.setTargetRel(-distance);
+	controller.move(stepper1, stepper2);
 }
