1ac8750d19ecd0e2bcc4d6458ca649ccb4e6d9f4
JonathanPlasse <13716151+JonathanPlasse@users.noreply.github.com>
2020-02-18 14:06:06 +0100

    Disabled Serial

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,15 @@ TurnAndGo turn_and_go;
 
 void setup() {
     // Setup serial link
-    Serial.begin(9600);
-    while(!Serial);
+    // Serial.begin(9600);
+    // while(!Serial);
     pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
     if (turn_and_go.run() == STOP) {
         delay(1000);
-        writeData(turn_and_go.getPosition(), sizeof(position_t));
+        // writeData(turn_and_go.getPosition(), sizeof(position_t));
         turn_and_go.goTo(distance, 0);
         distance = -distance;
     }
